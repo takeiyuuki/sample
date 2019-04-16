19fa91533742ec6c4e0d199d88e305b6562f6145
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-17 01:17:24 +0900

    データベースへの登録の条件分岐行なった。 private以下はストロングパラメータを行なった。

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,17 @@ class ContactsController < ApplicationController
   end
 
   def create
-    Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content]) # モデルにDB操作の命令を出す
-    redirect_to new_contact_path
+     # Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content]) # モデルにDB操作の命令を出す
+     @contact = Contact.create(contact_params)
+     if @contact.save
+    redirect_to new_contact_path, notice:"ブログを作成しました！"
+  else
+    render 'new'
   end
+end
+  private
+
+  def contact_params
+     params.require(:contact).permit(:name, :email, :content)
+   end
 end
